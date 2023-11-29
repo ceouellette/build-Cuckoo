89957d24030bfb662a0308b4879321e7d5fce436
Cole Ouellette <cole.ouellette17@gmail.com>
2023-11-29 11:53:44 -0500

    Fixed issue with missing "-y" flag

--- build.sh
+++ build.sh
@@ -20,18 +20,18 @@ banner_message="
 "
 echo -e "$banner_message"
 # Required Python libraries to get Cuckoo installed and running properly
-sudo apt install python python-pip python-dev libffi-dev libssl-dev
-sudo apt install python-virtualenv python-setuptools
-sudo apt install libjpeg-dev zlib1g-dev swig
-sudo apt install m2crypto
+sudo apt -y install python python-pip python-dev libffi-dev libssl-dev
+sudo apt -y install python-virtualenv python-setuptools
+sudo apt -y install libjpeg-dev zlib1g-dev swig
+sudo apt -y install m2crypto
 # Required to use the Django-based Web Interface
-sudo apt install mongodb
+sudo apt -y install mongodb
 # Cuckoo recommends we use PostgreSQL as our database
-sudo apt install postgresql libpq-dev
+sudo apt -y install postgresql libpq-dev
 # Oracle VirtualBox will be used to manage our VMs
-sudo apt install virtualbox
+sudo apt -y install virtualbox
 # Tcpdump
-sudo apt install tcpdump apparmor-utils
+sudo apt -y install tcpdump apparmor-utils
 
 banner_message="
 ************************************************
@@ -60,10 +60,10 @@ banner_message="
 ************************************************
 "
 echo -e "$banner_message"
-sudo apt install virtualenv
-sudo apt install virtualenvwrapper 
+sudo apt -y install virtualenv
+sudo apt -y install virtualenvwrapper 
 echo "source /usr/share/virtualenvwrapper/virtualenvwrapper.sh" >> ~/.bashrc
-sudo apt install python3-pip
+sudo apt -y install python3-pip
 pip3 completion --bash >> ~/.bashrc
 sudo pip3 install --user virtualenvwrapper
 echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python" >> ~/.bashrc
