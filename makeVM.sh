5c1c323fce1a8e8321aee1f61a41607d02f24034
Cole Ouellette <cole.ouellette17@gmail.com>
2023-11-29 21:23:22 -0500

    Added support for creating Win7 VMs

--- makeVM.sh
+++ makeVM.sh
@@ -1,5 +1,121 @@
 #!/bin/bash
 
+win7function () {
+    while true; do
+        read -p "Enter Base VM Name: " baseName
+        if ! [[ "$baseName" =~ \  ]]; then
+            break
+        else
+            echo "Base VM Name cannot contain spaces"
+            unset baseName
+        fi
+    done
+
+    while true; do
+        read -p "Enter Clone VM Name: " cloneName
+        if ! [[ "$cloneName" =~ \  ]]; then
+            break
+        else
+            echo "Clone VM Name cannot contain spaces"
+            unset cloneName
+        fi
+    done
+
+    while true; do
+        read -p "How many CPUs? " cpus
+        if [[ $cpus =~ ^[0-9]+$ ]]; then
+            break
+        else
+            echo "Invalid CPU count"
+            unset cpus
+        fi
+    done
+
+    while true; do
+        read -p "How much RAM (MB)? " ramsize
+        if [[ $ramsize =~ ^[0-9]+$ ]]; then
+            break
+        else
+            echo "Invalid RAM size"
+            unset ramsize
+        fi
+    done
+
+    read -p "What dependencies do you want installed? (Separate multiple dependencies with spaces): " deps
+
+    while true; do
+        read -p "How many Snapshots? " snapshotCount
+        if [[ $snapshotCount =~ ^[0-9]+$ ]]; then
+            break
+        else
+            echo "Invalid Snapshot Count"
+            unset snapshotCount
+        fi
+    done
+
+    read -p "What network IP should the Snapshots begin at? (IPv4 address): " networkIP
+
+    if ! [ -f ~/win7x64.iso ]; then
+        banner_message="
+************************************************
+*                                              *
+*            Downloading Win7x64 iso           *
+*                                              *
+************************************************
+"
+        echo -e "$banner_message"
+        wget https://archive.org/download/Windows7-iso/win7_64_bit.iso -O ~/win7x64.iso
+    fi
+    
+    banner_message="
+************************************************
+*                                              *
+*             Initializing Base VM             *
+*                                              *
+************************************************
+"
+    echo -e "$banner_message"
+    sudo mkdir /mnt/win7
+    sudo chown cuckoo:cuckoo /mnt/win7
+    sudo mount -o ro,loop ~/win7x64.iso /mnt/win7
+
+    vmcloak init --win7x64 $baseName --cpus $cpus --ramsize $ramsize
+
+    banner_message="
+************************************************
+*                                              *
+*          Cloning VM for Modification         *
+*                                              *
+************************************************
+"
+    echo -e "$banner_message"
+    vmcloak clone $baseName $cloneName
+
+        banner_message="
+************************************************
+*                                              *
+*            Installing Dependencies           *
+*                                              *
+************************************************
+"
+    echo -e "$banner_message"
+    vmcloak install $cloneName $deps
+    
+    banner_message="
+************************************************
+*                                              *
+*              Creating Snapshots              *
+*                                              *
+************************************************
+"
+    echo -e "$banner_message"
+    vmcloak snapshot --count $snapshotCount $cloneName $networkIP
+}
+
+win10function () {
+    echo "Windows 10 is not yet supported as of now"
+}
+
 banner_message="
 ************************************************
 *                                              *
@@ -8,12 +124,33 @@ banner_message="
 ************************************************
 "
 echo -e "$banner_message"
-virtualenv ~/cuckoo
+virtualenv -p python2.7 ~/cuckoo
 . ~/cuckoo/bin/activate
 
-pip install -y -U pip setuptools
-pip install -y -U cuckoo
+pip install -U pip setuptools
+pip install -U cuckoo
 sudo apt -y install git
-git clone https://github.com/ceouellette/VMCloak && cd VMCloak
-pip install .
+git clone https://github.com/ceouellette/VMCloak
+cd VMCloak
+python setup.py install
+cd ..
 vmcloak-vboxnet0
+
+while true; do
+    read -p "What Windows version do you want to install? (win7 / win10): " version
+    case $version in
+        win7)
+            win7function
+            break
+            ;;
+        win10)
+            win10function
+            ;;
+         *)
+            echo 'Invalid version input'
+            unset version
+    esac
+done
+
+
+
